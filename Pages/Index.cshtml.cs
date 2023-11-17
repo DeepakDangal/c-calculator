f79a4f21c5dc13643c0eddd90d32372b5f5d3f4d
Deepak Dangal <deepak@example.com>
2023-11-17 09:59:48 -0500

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab1app.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+    public IActionResult OnPost(string operation, double value1, double value2)
+    {
+        double result = 0;
+
+        if (operation == "add")
+        {
+            result = value1 + value2;
+        }
+        else if (operation == "subtract")
+        {
+            result = value1 - value2;
+        }
+        else if (operation == "multiply")
+        {
+            result = value1 * value2;
+        }
+        else if (operation == "power")
+        {
+            result = Math.Pow(value1, value2);
+        }
+        else if(operation == "AC"){
+            result = 0;
+            ViewData["display"] = "";
+            return Page();
+        }
+        else if(operation == "cube"){
+            result = value1*value1*value1;
+        }
+        else if(operation =="square"){
+            result = value1*value1;
+        }
+        else
+        {
+            ViewData["display"] = "Invalid operation";
+            return Page();
+        }
+
+        ViewData["display"] = result.ToString();
+
+        return Page();
+    }
+    
+}
